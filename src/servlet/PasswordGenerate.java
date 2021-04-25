2eecb452de03ed598078c4a495e563fabeb09f88
longunder <48555380+longunder@users.noreply.github.com>
2021-04-26 00:40:52 +0900

    インデントを修正する

--- src/servlet/PasswordGenerate.java
+++ src/servlet/PasswordGenerate.java
@@ -15,28 +15,28 @@ import model.PasswordGenerateLogic;
 
 @WebServlet("/PasswordGenerate")
 public class PasswordGenerate extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // フォワードする
-    RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
-    dispatcher.forward(request, response);
-  }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// フォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
+		dispatcher.forward(request, response);
+	}
 
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	Password password = new Password();
-    PasswordGenerateLogic passwordGenerateLogic = new PasswordGenerateLogic();
-    try {
-    	// ランダムな英数字をパスワードに設定する
-    	passwordGenerateLogic.execute(password);
-    	// リクエストスコープに保存する
-    	request.setAttribute("password", password);
-    } catch (NoSuchAlgorithmException e) {
-    	e.printStackTrace();
-    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Password password = new Password();
+		PasswordGenerateLogic passwordGenerateLogic = new PasswordGenerateLogic();
+		try {
+			// ランダムな英数字をパスワードに設定する
+			passwordGenerateLogic.execute(password);
+			// リクエストスコープに保存する
+			request.setAttribute("password", password);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 
-    // フォワードする
-    RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
-    dispatcher.forward(request, response);
-  }
+		// フォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+		dispatcher.forward(request, response);
+	}
 }
